6e546af949b8af8004b98e278eaba70e165215bb
Diego Cotte <diegocotte1407@gmail.com>
2022-12-16 21:33:10 -0500

    First Deploy

---
+++ src/containers/BaseLayout.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useMachine } from '@xstate/react';
+import { Nav } from '../components/Nav';
+import { StepsLayout } from './StepsLayout';
+import bookingMachine from '../machines/bookingMachine';
+import './BaseLayout.css';
+
+export const BaseLayout = () => {
+  const [state, send] = useMachine(bookingMachine);
+
+  console.log('nuestra maquina', state.context);
+
+  return (
+    <div className='BaseLayout'>
+      <Nav state={state} send={send} />
+      <StepsLayout state={state} send={send}/>
+    </div>
+  );
+}
---
+++ src/machines/bookingMachine.js
@@ -0,0 +1,117 @@
+import { assign, createMachine } from "xstate";
+import { fetchCountries } from "../utils/api";
+
+const fillCountries = {
+  initial: "loading",
+  states: {
+    loading: {
+      invoke: {
+        id: "getCountries",
+        src: () => fetchCountries,
+        onDone: {
+          target: "success",
+          actions: assign({
+            countries: (context, event) => event.data,
+          }),
+        },
+        onError: {
+          target: "failure",
+          actions: assign({
+            error: "Fallo el request",
+          }),
+        },
+      },
+    },
+    success: {},
+    failure: {
+      on: {
+        RETRY: { target: "loading" },
+      },
+    },
+  },
+};
+
+const bookingMachine = createMachine(
+  {
+    predictableActionArguments: true,
+    id: "buy plane tickets",
+    initial: "initial",
+    context: {
+      passengers: [],
+      selectedCountry: "",
+      countries: [],
+      error: "",
+    },
+    states: {
+      initial: {
+        on: {
+          START: {
+            target: "search",
+            actions: "printTransition",
+          },
+        },
+      },
+      search: {
+        entry: "printEntry",
+        exit: "printExit",
+        on: {
+          CONTINUE: {
+            target: "passengers",
+            actions: assign({
+              selectedCountry: (context, event) => event.selectedCountry,
+            }),
+          },
+          CANCEL: "initial",
+        },
+        ...fillCountries,
+      },
+      passengers: {
+        on: {
+          DONE: {
+            target: "tickets",
+            cond: "moreThanOnePassenger",
+          },
+          CANCEL: {
+            target: "initial",
+            actions: "cleanContext",
+          },
+          ADD: {
+            target: "passengers",
+            actions: assign((context, event) =>
+              context.passengers.push(event.newPassenger)
+            ),
+          },
+        },
+      },
+      tickets: {
+        after: {
+          5000: {
+            target: "initial",
+            actions: "cleanContext",
+          },
+        },
+        on: {
+          FINISH: "initial",
+        },
+      },
+    },
+  },
+  {
+    actions: {
+      cleanContext: (context) => {
+        context.passengers = [];
+        context.selectedCountry = "";
+      },
+      printTransition: () => console.log("Transición entre welcome y search"),
+      printEntry: () => console.log("Entrando al search"),
+      printExit: () => console.log("Saliendo del search"),
+    },
+    guards: {
+      moreThanOnePassenger: (context) => {
+        return context.passengers.length > 0;
+      }
+    }
+  }
+);
+
+export default bookingMachine;
